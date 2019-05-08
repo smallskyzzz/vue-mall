c83cf51597f1d481c7d93b3f59c47b683ee2238a
zts <247931368@qq.com>
2019-05-08 12:02:05 +0800

    新增：基本布局及导航切换

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,28 @@
     <!--顶部header-->
     <mt-header fixed="" title="vue-mall"></mt-header>
     <!--router-view-->
-    <router-view></router-view>
+    <transition>
+      <router-view></router-view>
+    </transition>
     <!--tab-->
     <nav class="mui-bar mui-bar-tab">
-      <a class="mui-tab-item mui-active" href="#tabbar">
+      <router-link to="/home" class="mui-tab-item">
         <span class="mui-icon mui-icon-home"></span>
         <span class="mui-tab-label">首页</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-chat">
-        <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
-        <span class="mui-tab-label">消息</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-contact">
+      </router-link>
+      <router-link to="/member" class="mui-tab-item">
         <span class="mui-icon mui-icon-contact"></span>
-        <span class="mui-tab-label">通讯录</span>
-      </a>
-      <a class="mui-tab-item" href="#tabbar-with-map">
-        <span class="mui-icon mui-icon-gear"></span>
-        <span class="mui-tab-label">设置</span>
-      </a>
+        <span class="mui-tab-label">会员</span>
+      </router-link>
+      <router-link to="/shopcar" class="mui-tab-item">
+        <span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">9</span></span>
+        <span class="mui-tab-label">购物车</span>
+      </router-link>
+      <router-link to="search" class="mui-tab-item">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
+      </router-link>
     </nav>
-    <h1>App组件</h1>
   </div>
 </template>
 
@@ -34,7 +35,21 @@ export default {
 </script>
 
 <style lang="less" scoped>
-#app{
-  padding-top: 40px;
-}
+  #app{
+    padding-top: 40px;
+    overflow-x: hidden;
+  }
+
+  .v-enter,.v-leave-to{
+    transform: translateX(100%);
+    opacity: 0;
+  }
+  .v-enter-active{
+    transition: all .5s ease;
+  }
+  // 瞬间不见，否则动画效果会有错乱
+  // 或者将leave-to拿出来加上absolute
+  .v-leave-active{
+    transition: all 0s ease;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '../components/home'
+import Member from '../components/member'
+import Shopcar from '../components/shopcar'
+import Search from '../components/search'
 
 Vue.use(Router)
 
 export default new Router({
-
+  routes: [
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/member',
+      component: Member
+    },
+    {
+      path: '/shopcar',
+      component: Shopcar
+    },
+    {
+      path: '/search',
+      component: Search
+    }
+  ],
+  // 覆盖默认的路由高亮类
+  linkActiveClass: 'mui-active'
 })
